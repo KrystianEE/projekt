5cce7fccca79c1d618217e197658e527e6b02e20
Krystian <kujawski.krystian@gmail.com>
2018-05-25 13:58:49 +0200

    Fixed spelling errors

--- kupiekota/kupiekota/urls.py
+++ kupiekota/kupiekota/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

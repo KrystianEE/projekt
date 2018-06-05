97ff85d8abba9d9f94ca6852619b6621fcfc0863
Krystian <kujawski.krystian@gmail.com>
2018-06-05 21:38:43 +0200

    Fixed messages and spelling

--- kupiekota/usermsg/urls.py
+++ kupiekota/usermsg/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'usermsg'
 
 urlpatterns = [
-    path('<username>/', views.UserBoxListView.as_view(), name='userboxlist'),
+    path('', views.UserBoxListView.as_view(), name='userboxlist'),
     path('<username>/<int:pk>/', views.detail_box, name='boxdetail'),
     path('new/<post_user>/', views.create_box, name='new_box'),
 ]
